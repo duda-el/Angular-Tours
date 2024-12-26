7cc6a8fbf91cb7e71afe7b4d6ea376e4c00357d3
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-12-26 14:10:17 +0400

    design update on checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,31 +1,34 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HotelService } from '../../services/hotel.service';
-import {Hotel} from '../../models/hotel.model'
+import { Hotel } from '../../models/hotel.model';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css',
+  styleUrls: ['./checkout.component.css'],
 })
-export class CheckoutComponent {
-  reservedHotel: Hotel | undefined;
+export class CheckoutComponent implements OnInit {
+  reservedHotel: Hotel | null = null;
 
-  constructor(private route: ActivatedRoute, private hotelService: HotelService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private hotelService: HotelService
+  ) {}
 
   ngOnInit(): void {
     const title = this.route.snapshot.queryParamMap.get('title');
 
     if (title) {
       this.hotelService.getAllHotels().subscribe((hotels) => {
-        this.reservedHotel = hotels.find(
+        const hotel = hotels.find(
           (hotel) => this.slugify(hotel.title) === this.slugify(title)
         );
+        this.reservedHotel = hotel || null; // Ensure null if no match found
       });
     } else {
       console.error('No hotel title provided in query parameters.');
@@ -37,5 +40,11 @@ export class CheckoutComponent {
       .toLowerCase()
       .replace(/[\s\W-]+/g, '-')
       .replace(/^-+|-+$/g, '');
+  }
+
+  createStarArray(rating: number): number[] {
+    const maxStars = 5;
+    const validRating = Math.min(Math.max(rating || 0, 0), maxStars); // Ensure rating is between 0 and 5
+    return Array.from({ length: validRating }, (_, i) => i + 1);
   }
 }
